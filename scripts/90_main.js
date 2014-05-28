b9110537b15d0e920c33d37ebec20af4451d6cfa
Simone Cociancich <simone.coch@tin.it>
2014-05-29 01:36:31 +0200

    Fleshed out an actual project portfolio page. Changed site header. Modified homepage layout and bio section

--- scripts/90_main.js
+++ scripts/90_main.js
@@ -6,6 +6,15 @@ $(function ready()
 	  return false;
 	});
 
+	// Magnific popup
+	$('a[href$=".jpg"] > img').each(function(i,el) {
+		$(el).parent('a').addClass('mfp-zoom').magnificPopup({ type:'image' });
+	});
+	$('.media > a:first-child[href^="#"]').magnificPopup({
+		type: 'inline',
+		midClick: true
+	});
+
 	// Language switcher
 	$('#languages a').on('click', function(evt) {
 	  var lang = $(this).attr('href').substr(1);
